d3e62decb4039929528011696aec4e623498d4fa
SaraR-1 <SaraR-1@users.noreply.github.com>
2021-07-21 10:32:44 +0100

    debug

--- augmentation/datasets/custom/celeba_128.py
+++ augmentation/datasets/custom/celeba_128.py
@@ -125,7 +125,7 @@ def compute_celeba_dataset_len_single(y_variant, z_variant, y_label, z_label, da
 
     for y_t, z_t in entries_to_populate:
         dataset_subgroup = dataset.filter(lambda image, y, z: (y == y_t and z == z_t))
-        global GROUP_SIZE_DICTS[dataset_name]
+        globals()GROUP_SIZE_DICTS[dataset_name]
         GROUP_SIZE_DICTS[dataset_name][y_variant][z_variant][(y_t, z_t)] = count_util(dataset_subgroup)
 
 def compute_celeba_dataset_len(y_variant, z_variant, y_label, z_label, train_data, val_data, test_data):
