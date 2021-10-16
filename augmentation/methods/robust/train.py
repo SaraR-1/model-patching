c4a5944e09515f06bc40158e8425ca20a147e365
SaraR-1 <SaraR-1@users.noreply.github.com>
2021-10-16 11:36:45 +0100

    debug add new metric

--- augmentation/methods/robust/train.py
+++ augmentation/methods/robust/train.py
@@ -330,7 +330,7 @@ def _train_robust_model(train_generators,
         # print(f"Print Check: {BEST_CASE_VALIDATION}, {SAVE_BEST_CASE_TEST}")
         if split_name == "validation":
             # Compute metric of interest on VALIDATION
-            # import pdb;pdb.set_trace()
+            import pdb;pdb.set_trace()
             accuracy_ = [v[0].result().numpy() for v in subgroup_accuracy.values()]
             metric_of_interst = abs(max(accuracy_) - min(accuracy_))
             if metric_of_interst < BEST_CASE_VALIDATION:
