a2a7bb48bc3a420eadd36f9f7f8bd32bd8081310
SaraR-1 <SaraR-1@users.noreply.github.com>
2022-06-28 13:36:18 +0100

    debug

--- augmentation/datasets/custom/celeba_128.py
+++ augmentation/datasets/custom/celeba_128.py
@@ -44,9 +44,7 @@ CELEBA_BASE_VARIANTS = ['5_o_Clock_Shadow',
                         'Wearing_Necktie',
                         'Young']
 
-# CELEBA_BASE_VARIANTS_accepted = CELEBA_BASE_VARIANTS[1:]
-CELEBA_BASE_VARIANTS_accepted = CELEBA_BASE_VARIANTS
-# CELEBA_BASE_VARIANTS_accepted[0] = "o_Clock_Shadow"
+CELEBA_BASE_VARIANTS_accepted = CELEBA_BASE_VARIANTS[1:]
 # Init var names? Cannot use 5_o_Clock_Shadow as it starts with a number - not accepeted
 for x in CELEBA_BASE_VARIANTS_accepted:
     exec("%s = %d" % (x,100))
