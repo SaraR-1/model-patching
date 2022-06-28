8aef17f9e400a3338cb4b186d93de7ee9a1deb06
SaraR-1 <SaraR-1@users.noreply.github.com>
2022-06-28 09:32:30 +0100

    change seed

--- augmentation/datasets/custom/celeba_128.py
+++ augmentation/datasets/custom/celeba_128.py
@@ -271,6 +271,7 @@ def load_celeba_128(dataset_name, dataset_version, data_dir, undersampling_info)
                                                       (Blond_Hair == y_t and Male == z_t)).shuffle(buffer_size=shuffle_buffer, 
                                                                                                    seed=undersample_shuffle_seed)
         # Take out examples from Y = 0, Z = 0
+        import pdb; pdb.set_trace()
         train_dataset_y0z0 = train_dataset_y0z0.take(n_subgroup_examples)
         # Keep only examples from groups other than Y = 0, Z = 0
         # train_dataset = train_dataset.filter(lambda image, y, z: (y != 0 or z != 0))
